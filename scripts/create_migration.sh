c94afc0886f0887cdc886edd6539eec97eadac06
Jackie <li.jackie565@gmail.com>
2025-10-16 18:16:46 -0400

    init migrations

---
+++ scripts/create_migration.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# create_migration.sh creates a migration in the migrations directory
+
+if [ $# -eq 0 ]; then
+  echo "Error: No migration name provided."
+  echo "Usage: $0 <migration_name>"
+  exit 1
+fi
+
+migrate create -ext sql -dir migrations $1
